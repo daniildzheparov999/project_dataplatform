84c11ee68a09b140fbae7a5d5f7b37c544465df8
daniildzheparov999 <dgeparovdanil@gmail.com>
2022-12-13 17:36:28 +0300

    added application into docker

---
+++ application/back/EmployeeApp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Departments(models.Model):
+          DepartmentId = models.AutoField(primary_key=True)
+          DepartmentName = models.CharField(max_length=500)
+
+class Employees(models.Model):
+          EmployeeId = models.AutoField(primary_key=True)
+          EmployeeName = models.CharField(max_length=500)
+          Department = models.CharField(max_length=500)
+          DateOfJoining = models.DateField()
+          PhotoFileName = models.CharField(max_length=500)
+
+class Worklogs(models.Model):
+          WorklogId = models.AutoField(primary_key=True)
+          Department = models.CharField(max_length=500)
+          Employee = models.CharField(max_length=500)
+          WorklogHours = models.CharField(max_length=500)
---
+++ application/back/EmployeeApp/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from EmployeeApp.models import Departments, Employees, Worklogs
+
+class DepartmentSerializer(serializers.ModelSerializer):
+          class Meta:
+                    model=Departments
+                    fields=('DepartmentId', 'DepartmentName')
+
+class EmployeeSerializer(serializers.ModelSerializer):
+          class Meta:
+                    model=Employees
+                    fields=('EmployeeId', 'EmployeeName', 'Department', 'DateOfJoining', 'PhotoFileName')
+
+class WorklogSerializer(serializers.ModelSerializer):
+          class Meta:
+                    model=Worklogs
+                    fields=('WorklogId', 'Department', 'Employee', 'WorklogHours')
